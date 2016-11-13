fbd2eb17bf3c520ab8d4085c2a073e19f733615e
Artemis <eminik35@hotmail.com>
2016-11-13 12:37:15 +0000

    Add Notes on Clustering and Assignment 1

--- Module5/assignment1.py
+++ Module5/assignment1.py
@@ -2,11 +2,20 @@
 # TOOD: Import whatever needs to be imported to make this work
 #
 # .. your code here ..
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+from sklearn.cluster import KMeans
+
+## Changed to Python 3 from here on ##
+plt.style.use('ggplot') # Look Pretty
 
 
-matplotlib.style.use('ggplot') # Look Pretty
-
-
+# Change directory
+dataDirectory = "C:/Users/Pavlos-Dell/Desktop/Further Learning/edX/Programming with Python for Data Science/DAT210x/Module5"
+os.chdir(dataDirectory)
 #
 # TODO: To procure the dataset, follow these steps:
 # 1. Navigate to: https://data.cityofchicago.org/Public-Safety/Crimes-2001-to-present/ijzp-q8t2
@@ -31,17 +40,18 @@ def doKMeans(df):
   # since the remaining columns aren't really applicable for this purpose.
   #
   # .. your code here ..
-
+  df = df[["Longitude", "Latitude"]]
   #
   # TODO: Use K-Means to try and find seven cluster centers in this df.
   #
   # .. your code here ..
-
+  kmeans_model = KMeans(n_clusters=7)
+  kmeans_model.fit(df)
   #
   # INFO: Print and plot the centroids...
   centroids = kmeans_model.cluster_centers_
   ax.scatter(centroids[:,0], centroids[:,1], marker='x', c='red', alpha=0.5, linewidths=3, s=169)
-  print centroids
+  print(centroids)
 
 
 
@@ -50,28 +60,33 @@ def doKMeans(df):
 #
 # .. your code here ..
 
+gamblingData = pd.read_csv("Datasets/gambling_crimes.csv")
+print(gamblingData.describe())
 
 #
 # TODO: Drop any ROWs with nans in them
 #
 # .. your code here ..
 
-
+gamblingData = gamblingData.dropna()
 #
 # TODO: Print out the dtypes of your dset
 #
 # .. your code here ..
 
-
+print(gamblingData.dtypes)
 #
 # Coerce the 'Date' feature (which is currently a string object) into real date,
 # and confirm by re-printing the dtypes. NOTE: This is a slow process...
 #
 # .. your code here ..
-
+gamblingData["Date"] = pd.to_datetime(gamblingData["Date"], format="%m/%d/%Y %I:%M:%S %p")
+print(gamblingData.dtypes)
+print(gamblingData.head())
 
 # INFO: Print & Plot your data
-doKMeans(df)
+doKMeans(gamblingData)
+
 
 
 #
@@ -80,11 +95,12 @@ doKMeans(df)
 # crime incidents, as well as a new K-Means run's centroids.
 #
 # .. your code here ..
-
-
+gamblingDataFiltered = gamblingData.loc[gamblingData.Date > '2011-01-01', :]
+print(gamblingData.shape)
+print(gamblingDataFiltered.shape)
 
 # INFO: Print & Plot your data
-doKMeans(df)
+doKMeans(gamblingDataFiltered)
 plt.show()
 
 
---
+++ module5_1_Clustering.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 29 18:11:05 2016
+
+@author: Artemis Nika
+"""
+
+
+import os
+
+
+#dataDir = "C:\Users\Pavlos-Dell\Desktop\Further Learning\edX\Programming with Python for Data Science\DAT210x\Module4"
+#os.chdir(dataDir)
+
+
+## K-Means
+# Clustering groups samples that are similarn within the same cluster. The more similar the samples belonging to 
+# a cluster group are (and conversely, the more dissimilar samples in separate groups), the better the clustering 
+# algorithm has performed.
+# K-means iteratively separates your samples into a user-specified number of K-cluster groups of roughly equal 
+# variance. Cluster groups are defined by their geometric cluster center -  a single point called the centroid.
+# Every sample in the dataset is assigned to the centroid nearest to it.
+# In the case of continuous features, calculating the distance is straightforward, but when you have categorical
+# features you'll have to come up with other methods.
+#
+
+## The K-means algorithm
+#  K-means starts by placing a user-specified number of K cluster centers in your feature space. There are many 
+# techniques fro choosing the first centroid placement and your results will vary depending on the one you select.
+# The simples way is to use the position of some random samples as the centroids' starting sports.
+#  Each cluster then takes ownership of the samples nearest to its centroid, and every sample can only be assigned
+# to one cluster. The distance metric usually used  is the n-dimensional Euclidean distance between the sample and 
+# the centroid. After all samples have been assigned to a cluster, the centroid location is updated to be the mean 
+# value of all samples assigned to it. The mean value is calculated by feature, so the centroid position ends up 
+# being a n-length vector within your feature space.
+#  The assignment and update repeats until there are no more changes in either, at which point the algorithm has
+# converged. K-means always converges but it does not always converge at the global minimum.
+#   The technical explanation for what K-means does is minimizing the sum of squared errors between each sample and
+# its respective centroid. As mentioned, the initial centroid assignment affects the results. Two runs of K-means
+# might produce different outcomes, but the quality of their cluster assignments are ranked by looking at which run 
+# has the smalles overall inertia.
+
+## K-means use
+#  K-Means clustering is best suited when you have a good idea of the number of distinct clusters your unlabeled dataset 
+# should be segmented into. Generally, the output of K-Means is used in two ways. To separate your unlabeled data into K 
+# groups, which is the clear use case, or to find and use the resulting centroids.
+#  You can use the centroid to 'compress' your data. By referring to the centroid rather than the data sample, the number
+# of unique values is reduced, which optimizes the execution speed of other algorithms. 
+import pandas as pd
+import numpy as np
+
+from sklearn.cluster import KMeans 
+from sklearn import preprocessing
+
+df = pd.DataFrame(np.random.randint(0,100,size=(100, 4)), columns=list('ABCD'))
+stand_df = preprocessing.scale(df)
+kmeans = KMeans(n_clusters=5)
+kmeans.fit(stand_df)
+
+labels = kmeans.predict(stand_df)
+centroids = kmeans.cluster_centers_
+
+
+## K-means Gotchas
+#  Considering how basic K-means is, it performs pretty well and its implementation is the basis of far more advanced
+# clustering algorithms, such as learning vector quantization and Gaussian mixture. Having a solid understanding of 
+# K-means will help you understand those better when you study them.
+#  K-means is only really suitable when you have a good estimate of the number of clusters that exist in your data.
+# Even if you do have the right number of clusters selected, the result produced by K-means can vary depending on the
+# initial centroid placement. Due to cenrtroid seed placement having som much of an effect on your clustering outcome, 
+# you have to be careful since it is possible to have centroid with only a single sample assigned to them, or even no
+# samples assigned to them.
+#  Two other key characteristics of K-means are that it assumes your samples are length normalized and as such is 
+# sensitive to featire scaling. It also assumes that the cluster sizes are rougly spherical and similar; this way
+# the nearest centroid is always the correct assignment.
